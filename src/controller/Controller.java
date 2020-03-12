81bcc00f34d9e2b46af627076bc68fc65a29ee0b
Cecilia <cecilia@192.168.1.129>
2020-03-11 23:47:58 -0500

    Todo heap

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -80,13 +80,15 @@ public class Controller {
 					String res = lector.next();
 					String[] tipos = res.split("-");
 					
+					long start2 = System.currentTimeMillis();
 					ArrayList<Comparendo> buscados = modelo.darNComparendosMasNorteHeap(numero, tipos);
 					
 					for(int i = 0; i < buscados.size(); i++)
 					{
 						view.printMessage(buscados.get(i).datosCluster2());
 					}
-					
+					long end2 = System.currentTimeMillis();
+					view.printMessage("Tiempo de carga (seg): " + (end2-start2)/1000.0 + "\n---------");
 					
 					break;
 
